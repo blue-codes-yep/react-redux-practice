a1ec5536095eaaad5a6ef9f114e16d61e1b29d7a
Blue Lindsey <linzblue5@gmail.com>
2020-04-07 14:56:38 -0500

    Added addCat Action.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { ACTION_SET_ACTIVITY, ACTION_SET_NAME } from './actionTypes';
+import { ACTION_SET_ACTIVITY, ACTION_SET_NAME, ACTION_ADD_CAT } from './actionTypes';
 
 export const setActivity = activity => ({
     type: ACTION_SET_ACTIVITY,
@@ -11,5 +11,12 @@ export const setName = name => ({
     type: ACTION_SET_NAME,
     payload: {
         name
+    }
+})
+
+export const addCat = cat => ({
+    type: ACTION_ADD_CAT,
+    payload: {
+        cat
     }
 })
