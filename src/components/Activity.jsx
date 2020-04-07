97f19e6c5e3cb57609b6379f7ea6aee08399eba3
Blue Lindsey <linzblue5@gmail.com>
2020-04-07 13:02:43 -0500

    Added UpdateName comonent into Acvitivy.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import { eat } from "../redux/actions"
-import { play } from "../redux/actions"
-import { nap } from "../redux/actions"
+import { setActivity } from "../redux/actions"
+import UpdateName from "./UpdateName"
 
 
-const Activity = ({ activity, eat, play, nap }) => (
+const Activity = ({ activity, name, setActivity }) => (
     <>
-
         <h1>What is the cat doing?</h1>
-        <p>The cat is {activity}</p>
-        <button onClick={() => eat()}>Eating</button>
-        <button onClick={() => play()}>Playing</button>
-        <button onClick={() => nap()}>Napping</button>
+        <p>{name} is {activity}</p>
+        <button onClick={() => setActivity('eating')}>Eating</button>
+        <button onClick={() => setActivity('playing')}>Playing</button>
+        <button onClick={() => setActivity('napping')}>Napping</button>
+        <UpdateName />
+
     </>
 )
+
 
 const mapStateToProps = state => {
     const { activity } = state;
@@ -22,5 +23,5 @@ const mapStateToProps = state => {
 }
 export default connect(
     mapStateToProps, // mapStateToProps - What do I want?
-    { eat, play, nap }) // mapDispatchToProps - What do I want to do?
+    { setActivity }) // mapDispatchToProps - What do I want to do?
     (Activity);
